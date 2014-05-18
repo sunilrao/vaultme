049304914bba75208e5376668b21654e503d11b3
Prasath Venkatraman <ask4prasath@gmail.com>
2014-05-19 00:25:33 +0530

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Guardstreet::Application.config.secret_key_base = 'c2d89e7954119548c5bcebfd5bfb20827e9acbae58da41038cdedc4a099ec8cab65669a5a6a02d536c76b2443fc61079cb08235d64b9cfdccf45a2b449b5e236'
